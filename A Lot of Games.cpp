78b1e335a02e311bb66fd4403b0429a8555f4667
Nikhil Sanjay Wagh <nikhilwagh.work@gmail.com>
2017-01-15 16:07:27 +0530

    Add files via upload

---
+++ A Lot of Games.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int LL;
+typedef long double ld;
+#define maxx 10000000000
+typedef vector<LL> v1;
+typedef vector<v1> v2;
+typedef pair<LL,LL> ii;
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define total(c) allocator(all(c),ll(0))
+//typedef pair<int,int> ii;
+typedef pair<ii,int> pii;
+LL ma=-1,mi=1000000000000000;
+inline LL gcd(LL a, LL b) {return b?gcd(b, a%b):a;}
+inline LL lcm(LL a, LL b, LL MOD) {return a/gcd(a,b) * b % MOD;}
+LL power(LL x, unsigned int y)
+{
+    if( y == 0)
+        return 1;
+    else if (y%2 == 0)
+        return power(x, y/2)*power(x, y/2);
+    else
+        return x*power(x, y/2)*power(x, y/2);
+}
+class node {
+public:
+        bool win,lose;
+        class node* a[27];
+        node()
+        {
+            win=false;
+            lose=false;
+            for(LL i=0;i<27;i++)
+            {
+                a[i]=NULL;
+            }
+        }
+};
+
+int main()
+{
+    /*freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);*/
+	ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+	LL i,j,k,flag,n,m,c=0,d=0,e[30]={0},x=0,t,temp=0,count;
+    string a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        string ans;
+            for(i=a.length()-1,j=b.length()-1;i>=0;i--,j--)
+        {
+            if(i>=j)
+                ans[i]=a[i]+c;
+            else
+                ans[j]=b[j]+c;
+            if((a[i]+b[j]-'a'-'a')>9)
+                c=1;
+            ans=(a[i]-'a'+b[j]-'a')%10;
+        }
+        if(c)
+            ans[max(i,j)]=1;
+        cout<<ans<<endl;
+    }
+
+}
+
---
+++ Alyona and mex.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int LL;
+typedef long double ld;
+#define maxx 10000000000
+typedef vector<LL> v1;
+typedef vector<v1> v2;
+typedef pair<LL,LL> ii;
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define total(c) allocator(all(c),ll(0))
+//typedef pair<int,int> ii;
+typedef pair<ii,int> pii;
+LL ma=-1,mi=1000000000000000;
+inline LL gcd(LL a, LL b) {return b?gcd(b, a%b):a;}
+inline LL lcm(LL a, LL b, LL MOD) {return a/gcd(a,b) * b % MOD;}
+LL power(LL x, unsigned int y)
+{
+    if( y == 0)
+        return 1;
+    else if (y%2 == 0)
+        return power(x, y/2)*power(x, y/2);
+    else
+        return x*power(x, y/2)*power(x, y/2);
+}
+
+int main()
+{
+    /*freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);*/
+	ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+	LL i,j,k,flag,n,m,c=0,d=0,e[30]={0},x=100000000000000,t,temp=0,count;
+    cin>>n>>m;
+    for(i=0;i<m;i++)
+    {
+        cin>>c>>d;
+        x=min(x,d-c);
+    }
+    x++;
+    cout<<x<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<i%(x)<<" ";
+    }
+
+
+}
+
